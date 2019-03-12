6815263c226dcb975f8a58825cc7085acd43bdcd
EC2 Default User <ec2-user@ip-172-31-29-211.ap-southeast-1.compute.internal>
2019-03-12 06:52:12 +0000

    Basic認証のtest実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :basic_auth
+    protect_from_forgery with: :exception
+    
+    private
+    
+    def basic_auth
+        authenticate_or_request_with_http_basic do |username, password|
+            username == "test" && password == "123456"
+        end
+    end
 end

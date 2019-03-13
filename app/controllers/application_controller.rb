75b199359732de1bcc181a74ed7e7439f1f257d0
EC2 Default User <ec2-user@ip-172-31-29-211.ap-southeast-1.compute.internal>
2019-03-13 08:01:07 +0000

    applicatio_controller.rb 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
     private
-    
     def basic_auth
-        http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+        authenticate_or_request_with_http_basic do |name, password|
+            name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+        end
     end
 end

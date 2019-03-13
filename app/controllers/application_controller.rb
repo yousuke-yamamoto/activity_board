c1aebd29ce05f13a32b40ac5f3d8eaa9a9817f8c
EC2 Default User <ec2-user@ip-172-31-29-211.ap-southeast-1.compute.internal>
2019-03-13 07:13:50 +0000

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
     private
     
     def basic_auth
-        authenticate_or_request_with_http_basic do |username, password|
-            username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-        end
+        http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
     end
 end
